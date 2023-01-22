c862fa940ebca3d34a27b92dfed042ca7e4851c0
Gilson Russo <44848212+gilsonrusso@users.noreply.github.com>
2023-01-22 01:24:13 +0000

    create a button styled-component to test

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Button } from "./components/Button";
 export function App() {
   return (
     <>
-      <Button color="primary" />
-      <Button color="secondary" />
-      <Button color="danger" />
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
       <Button />
     </>
   )
